111c4788aed6e2111c37e7f44210f6af808e647c
Nishad <nishadsingh@gmail.com>
2020-08-18 15:06:52 +0800

    v0.4.7

--- src/instructions.js
+++ src/instructions.js
@@ -11,7 +11,7 @@ import { PublicKey, TransactionInstruction } from '@solana/web3.js';
 import { TOKEN_PROGRAM_ID } from './token-instructions';
 
 export const DEX_PROGRAM_ID = new PublicKey(
-  'HLP6LmVgK16Bkpjgc4BdGmVNNc8m6pKSkdknbHxp2KMZ',
+  'DXgi6RmREQNFnWRV4gP28CQFdp4k8f6YSqGjH6fgJLDq',
 );
 
 export const INSTRUCTION_LAYOUT = new VersionedLayout(
