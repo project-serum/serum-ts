dc452b9f06f8b6bb4e5f08b8466e91e4b0ccb57d
philippe-ftx <62417741+philippe-ftx@users.noreply.github.com>
2020-09-11 16:47:34 +0200

    Export base and quote wallet key indexes for settle funds instruction (#16)
    Export base and quote wallet key indexes for settle funds instruction

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 export { Market, Orderbook, OpenOrders } from './market';
-export { DexInstructions, decodeInstruction } from './instructions';
+export {
+  DexInstructions,
+  decodeInstruction,
+  SETTLE_FUNDS_BASE_WALLET_INDEX,
+  SETTLE_FUNDS_QUOTE_WALLET_INDEX,
+} from './instructions';
 export { TOKEN_MINTS, MARKETS } from './tokens_and_markets';
 export { decodeEventQueue, decodeRequestQueue } from './queue';
 export * as TokenInstructions from './token-instructions';
