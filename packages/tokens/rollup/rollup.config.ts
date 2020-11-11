c1a70bca0bef9e4b262abd80d4c01a7222795728
Nathaniel Parke <nathaniel.parke@gmail.com>
2020-11-11 11:43:58 +0800

    Add tokens package (#7)
    * Adds in tokens package
    
    * Remove test

---
+++ packages/tokens/rollup/rollup.config.ts
@@ -0,0 +1,35 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import typescript from 'rollup-plugin-typescript2';
+import json from 'rollup-plugin-json';
+
+const pkg = require('../package.json');
+
+export default {
+  input: `src/index.ts`,
+  output: [
+    {
+      file: pkg.main,
+      name: 'index',
+      format: 'umd',
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+  external: [],
+  watch: {
+    include: 'src/**',
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    json(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    sourceMaps(),
+  ],
+};
---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,9 @@
+import MAINNET_TOKENS from "./mainnet-beta.json";
+import DEVNET_TOKENS from "./devnet.json";
+import TESTNET_TOKENS from "./testnet.json";
+
+export const TOKENS = {
+  'mainnet': MAINNET_TOKENS,
+  'devnet': DEVNET_TOKENS,
+  'testnet': TESTNET_TOKENS,
+};
