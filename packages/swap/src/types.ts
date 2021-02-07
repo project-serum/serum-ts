61fa7d8e76a9549ffc11151da43f335435131d79
bartosz-lipinski <264380+bartosz-lipinski@users.noreply.github.com>
2021-02-06 23:13:35 -0600

    feat: swap v2

--- packages/swap/src/types.ts
+++ packages/swap/src/types.ts
@@ -8,14 +8,28 @@ export interface TokenAccount {
   info: TokenAccountInfo;
 }
 
+export enum CurveType {
+  ConstantProduct = 0,
+  ConstantPrice = 1,
+  Stable = 2,
+  ConstantProductWithOffset = 3,
+}
+
 export interface PoolConfig {
-  curveType: 0 | 1;
-  tradeFeeNumerator: number;
-  tradeFeeDenominator: number;
-  ownerTradeFeeNumerator: number;
-  ownerTradeFeeDenominator: number;
-  ownerWithdrawFeeNumerator: number;
-  ownerWithdrawFeeDenominator: number;
+  curveType: CurveType;
+  fees: {
+    tradeFeeNumerator: number;
+    tradeFeeDenominator: number;
+    ownerTradeFeeNumerator: number;
+    ownerTradeFeeDenominator: number;
+    ownerWithdrawFeeNumerator: number;
+    ownerWithdrawFeeDenominator: number;
+    hostFeeNumerator: number;
+    hostFeeDenominator: number;
+  };
+
+  token_b_offset?: number;
+  token_b_price?: number;
 }
 
 export interface PoolOptions {
